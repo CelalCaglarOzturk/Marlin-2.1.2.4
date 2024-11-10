e9afd2828ad2fd7fffc3f59f941e9ef5ab764000
Celal Çağlar Öztürk <celalcaglar34@gmail.com>
2024-11-10 15:17:20 +0300

    switch position set

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1514,7 +1514,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -15, -5 } // BUNU OLC
+#define NOZZLE_TO_PROBE_OFFSET { -42, -16, -5 } // BUNU OLC
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
